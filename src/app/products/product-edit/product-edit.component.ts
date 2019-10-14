4d4db8bc26092cbe515e7ccade6f642bef6891de
Yuniel Garcia <garciayuni84@gmail.com>
2019-10-14 11:59:52 -0400

    Add initial project setup.

---
+++ src/app/products/product-edit/product-edit.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { MessageService } from '../../messages/message.service';
+
+import { Product, ProductResolved } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit {
+  pageTitle = 'Product Edit';
+  errorMessage: string;
+
+  product: Product;
+
+  constructor(private productService: ProductService,
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+    ngOnInit() {
+      this.route.data.subscribe(data => {
+        const resolvedData: ProductResolved = data['resolvedData'];
+        this.errorMessage = resolvedData.error;
+        this.onProductRetrieved(resolvedData.product);
+      });
+    }
+
+  onProductRetrieved(product: Product): void {
+    this.product = product;
+
+    if (!this.product) {
+      this.pageTitle = 'No product found';
+    } else {
+      if (this.product.id === 0) {
+        this.pageTitle = 'Add Product';
+      } else {
+        this.pageTitle = `Edit Product: ${this.product.productName}`;
+      }
+    }
+  }
+
+  deleteProduct(): void {
+    if (this.product.id === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete(`${this.product.productName} was deleted`);
+    } else {
+      if (confirm(`Really delete the product: ${this.product.productName}?`)) {
+        this.productService.deleteProduct(this.product.id)
+          .subscribe(
+            () => this.onSaveComplete(`${this.product.productName} was deleted`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    }
+  }
+
+  saveProduct(): void {
+    if (true === true) {
+      if (this.product.id === 0) {
+        this.productService.createProduct(this.product)
+          .subscribe(
+            () => this.onSaveComplete(`The new ${this.product.productName} was saved`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      } else {
+        this.productService.updateProduct(this.product)
+          .subscribe(
+            () => this.onSaveComplete(`The updated ${this.product.productName} was saved`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    } else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+  }
+
+  onSaveComplete(message?: string): void {
+    if (message) {
+      this.messageService.addMessage(message);
+    }
+
+    // Navigate back to the product list
+    this.router.navigate(['/products']);
+  }
+}
